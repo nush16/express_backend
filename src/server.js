1d72bfacc0363834be98b178c1c3e419c820f221
Anushke <115445200+nush16@users.noreply.github.com>
2023-08-06 00:28:53 +1000

    changed prod name

--- src/server.js
+++ src/server.js
@@ -6,12 +6,12 @@ dotenv.config();
 const express = require("express");
 const app = express();
 
-// Configure host and port & provide default values
-const HOST = process.env.HOST || "localhost";
-const PORT = process.env.PORT || 3001;
-
 // Import mongoose
 const mongoose = require("mongoose");
+
+// Configure host and port & provide default values
+const HOST = process.env.HOST || "127.0.0.1";
+const PORT = process.env.PORT || 3001;
 
 // Configure helmet
 const helmet = require("helmet");
@@ -29,7 +29,7 @@ app.use(
 // Configure CORS
 const cors = require("cors");
 var corsOptions = {
-  origin: ["http://localhost:3001", "https://docgo2.netlify.app", ""],
+  origin: ["http://localhost:3001", "https://docgo2.netlify.app"],
   optionsSuccessStatus: 200,
 };
 app.use(cors(corsOptions));
@@ -42,16 +42,16 @@ app.use(express.json());
 app.use(express.urlencoded({ extended: true }));
 
 //Set db url
-var databaseURL = "";
+let databaseURL = "";
 switch (process.env.NODE_ENV.toLowerCase()) {
-  case "test":
-    databaseURL = "mongodb://localhost:27017/docgo_db_test"; // Test database URL
+  case "production":
+    databaseURL = process.env.DATABASE_URL; // Production database URL
     break;
   case "development":
     databaseURL = "mongodb://localhost:27017/docgo_db"; // Development database URL
     break;
-  case "production":
-    databaseURL = process.env.DATABASE_URL; // Production database URL
+  case "test":
+    databaseURL = "mongodb://localhost:27017/docgo_db_test"; // Test database URL
     break;
   default:
     console.error(
@@ -76,14 +76,17 @@ app.get("/databaseHealth", (request, response) => {
 });
 
 // Connect to the db
-async function dbConnect() {
-  try {
-    await mongoose.connect(databaseURL);
-    console.log("Database connected!");
-  } catch (error) {
-    console.log(`dbConnect failed, error:`, error);
-  }
-}
+const { databaseConnector } = require("./database");
+databaseConnector(databaseURL)
+  .then(() => {
+    console.log("Database connected successfully!");
+  })
+  .catch((error) => {
+    console.log(`
+    error occurred connecting to the database! It was: 
+    ${error}
+    `);
+  });
 
 dbConnect();
 
@@ -106,7 +109,7 @@ app.use("/appointments", appointmentRouter);
 // Routes
 app.get("/", (request, response) => {
   response.json({
-    message: "Hello world!",
+    message: "welcome!",
   });
 });
 
