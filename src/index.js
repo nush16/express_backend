55188a8895a8e65e9c3f87e90774d885df9f5719
Anushke <115445200+nush16@users.noreply.github.com>
2023-08-06 01:42:26 +1000

    fix error in index.js

--- src/index.js
+++ src/index.js
@@ -1,8 +1,8 @@
 // Import necessary modules and constants
-const { app, PORT, HOST } = require("./server");
+const { app, PORT } = require("./server");
 
 // Starting the server and listening on the specified host and port
-app.listen(PORT, HOST, () => {
+app.listen(PORT, () => {
   // function will be executed when the server starts successfully
   console.log(`
   Server is running on http://${PORT}
