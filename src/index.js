b30c76a07845912168f9f2b403fe95747a126298
Anushke <115445200+nush16@users.noreply.github.com>
2023-08-05 23:48:22 +1000

    removed host from index.js

--- src/index.js
+++ src/index.js
@@ -1,26 +1,10 @@
 // Import necessary modules and constants
-var { app, PORT, HOST } = require("./server");
+var { app, PORT } = require("./server");
 
 // Starting the server and listening on the specified host and port
-app.listen(PORT, HOST, () => {
+app.listen(PORT, () => {
   // function will be executed when the server starts successfully
   console.log(`
-  Server is running on http://${HOST}:${PORT}
+  Server is running on http://${PORT}
     `);
 });
-
-// Import necessary modules and constants
-// var { app, PORT, HOST } = require("./server");
-// const seedData = require("./seeds");
-
-// // Seed data into the database
-// seedData()
-//   .then(() => {
-//     // Start the server after seeding the data
-//     app.listen(PORT, HOST, () => {
-//       console.log(`Server is running on http://${HOST}:${PORT}`);
-//     });
-//   })
-//   .catch((error) => {
-//     console.error("Error seeding data:", error);
-//   });
